54a31db58c317a747e75a94b18bb5664b1b39746
Pedro Carmona <pedro.carmona@williamhill.com>
2018-10-04 10:12:15 +0200

    fetching api data with componentDidMount()

--- src/MyIp.js
+++ src/MyIp.js
@@ -1,24 +1,33 @@
 import React, {Component} from 'react';
 
-// const MyIp = () => {
-
 class MyIp extends Component {
 
-    getIpData = async() => {
-      const api_call = await fetch('https://api.ipify.org/?format=json');
-      const data = await api_call.json()
-      console.log('data = ',data)
-      return data
-    }
+  constructor(props){
+      super(props);
+      this.state = {
+          data:''
+      };
+  }
 
-    render() {
-      const { data } = this.getIpData();
-      return (
+  componentDidMount(){
+      fetch('https://api.ipify.org/?format=json')
+      .then(res => res.json())
+      .then(res => {
+      this.setState({
+        data:res.ip
+      })
+    })
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
         <div>
           Ip: {data}
         </div>
-      )
-    }
+    );
   }
+  
+}
 
 export default MyIp;
